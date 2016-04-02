a608d067c6d13fb48e959c73c47c3ed1dc84dd70
Katie <corcorankt1@gmail.com>
2016-04-02 11:18:10 -0400

    Update shoppinglist.js

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,16 +1,5 @@
 
-function addItem(){
-	var itemName = document.getElementById("itemName").value;
-	var itemPrice = document.getElementById("price").value;
-	var x = {name: itemName, price: itemPrice};
-	items.push(x);
-	console.log(items);
-	document.getElementById("itemName").value = "";
-	document.getElementById("price").value = "";
-};
-
-	var total = 0;
-	var items = [
+var items = [
 	{
 		name: "milk",
 		price: 3.00
@@ -28,15 +17,37 @@ function addItem(){
 		price: 5.00
 	},
 ]
-	items.forEach(function(element){
-	var newP = document.createElement('p');
-	newP.innerHTML = element.name + " $" + element.price.toFixed(2);
-	document.body.appendChild(newP);
+
+var total = 0;
+
+items.forEach(function(element){
+	var newLi = document.createElement('li');
+	var listItem = document.getElementById("item");
+	newLi.innerHTML = element.name + " $" + element.price.toFixed(2);
+	listItem.appendChild(newLi);
 	total += element.price;
 });
-	var newP = document.createElement('p');
+
+var newP = document.createElement('p');
 newP.innerHTML = "Total: $" + total.toFixed(2);
 document.body.appendChild(newP);
+
+function addItem(){
+	var itemName = document.getElementById("itemName").value;
+	var itemPrice = document.getElementById("price").value;
+	var numPrice = Number(itemPrice);
+	var x = {name: itemName, price: numPrice};
+	items.push(x);
+	document.getElementById("itemName").value = "";
+	document.getElementById("price").value = "";
+	var newLi = document.createElement('li');
+	var listItem = document.getElementById("item");
+	newLi.innerHTML = itemName + " $" + numPrice.toFixed(2);
+	listItem.appendChild(newLi);
+	total += numPrice;
+	console.log(total);
+	var newPrice = document.createTextNode("Total: $" + total.toFixed(2));
+};
 
 
 
